c2f185472a5c31943b0808ba536a025abae152bf
Chaminda Indunil <chaminda.indunil@hotmail.com>
2023-01-03 07:11:44 +0530

    add transaction object to list

--- ASD_CW_2/Program.cs
+++ ASD_CW_2/Program.cs
@@ -5,17 +5,113 @@ namespace ASD_CW_2
 {
     class Program
     {
+        private static List<Transaction> transactions = new List<Transaction>();
+        private static List<Category> categories = new List<Category>();
+
         static void Main(string[] args)
         {
-            List<Transaction> transactions = new List<Transaction>();
+            menu();
+        }
 
-            Transaction t1 = new Transaction(2500.00, "Salary", true, Convert.ToDateTime("2022-12-25"));
+        private static void menu()
+        {
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Add Transaction");
+            Console.WriteLine("2) List Transactions");
+            Console.WriteLine("3) Edit Transaction");
+            Console.WriteLine("4) Delete Transaction");
+            Console.WriteLine("5) Add Category");
+            Console.WriteLine("6) List Category");
 
-            Console.WriteLine(t1.getId());
+            Console.Write("Select an option: ");
 
-            Transaction t2 = new Transaction(250.00, "Food", false, Convert.ToDateTime("2022-12-30"));
+            try
+            {
+                int userInput = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(t2.getId());
+                switch (userInput)
+                {
+                    case 1:
+                        addTransaction();
+                        menu();
+                        break;
+                    case 2:
+                        listTransaction();
+                        menu();
+                        break;
+                    case 3:
+                        editTransaction();
+                        menu();
+                        break;
+                    case 4:
+                        deleteTransaction();
+                        menu();
+                        break;
+                    case 5:
+                        addCategory();
+                        menu();
+                        break;
+                    case 6:
+                        listCategory();
+                        menu();
+                        break;
+                    case 7:
+                        listCategory();
+                        menu();
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        throw new NotImplementedException();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void addTransaction()
+        {
+            Console.Write("Enter transaction amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter description: ");
+            string? desc = Console.ReadLine();
+            Console.Write("Is recurring (true or false): ");
+            bool recurring = Convert.ToBoolean(Console.ReadLine());
+            Console.Write("Enter due-date (yyyy.mm.dd): ");
+            DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+            Transaction t1 = new Transaction(amount, desc, recurring, date);
+
+            transactions.Add(t1);
+        }
+
+        private static void editTransaction()
+        {
+
+        }
+
+        private static void listTransaction()
+        {
+            foreach (Transaction t in transactions)
+            {
+                Console.WriteLine($"{t.getId()} : {t.getDate()} : {t.getAmount()} : {t.getDescription()} : {t.isRecurring()}");
+            }
+        }
+
+        private static void deleteTransaction()
+        {
+
+        }
+
+        private static void addCategory()
+        {
+
+        }
+
+        private static void listCategory()
+        {
+
         }
     }
 }
