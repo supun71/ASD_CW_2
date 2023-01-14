c2e8b3e64c25cff1d7effb7e63f1277b848847c5
Chaminda Indunil <chaminda.indunil@hotmail.com>
2023-01-14 11:09:10 +0530

    added all comments

--- ASD_CW_2/Program.cs
+++ ASD_CW_2/Program.cs
@@ -123,7 +123,7 @@ namespace ASD_CW_2
                 }
 
                 // Get category object related id from category list
-                Category? category = categories.FirstOrDefault(Category => Category.getId().Equals(categoryId));
+                Category category = categories.FirstOrDefault(Category => Category.getId().Equals(categoryId));
 
                 Console.Write("Is recurring (true or false): ");
                 bool recurring = Convert.ToBoolean(Console.ReadLine());
@@ -173,6 +173,8 @@ namespace ASD_CW_2
 
                 // transaction object added to the list
                 transactions.Add(t1);
+
+                Console.WriteLine("Primary Income Added Successfully");
             }
             catch (Exception ex)
             {
@@ -188,13 +190,15 @@ namespace ASD_CW_2
                 Console.Write("Enter the transaction Id: ");
                 int id = Convert.ToInt32(Console.ReadLine());
 
-                // Get transaction object from list
-                Transaction transactionObj = transactions.FirstOrDefault(Transaction => Transaction.getId() == id);
-
-                if (transactionObj == null)
+                // Check transaction is exist or not
+                if (!transactions.Any(transaction => transaction.getId().Equals(id)))
                 {
                     Console.WriteLine($"Id number {id} not belongs to any transaction");
+                    menu();
                 }
+
+                // Get transaction object from list
+                Transaction transactionObj = transactions.FirstOrDefault(Transaction => Transaction.getId() == id);
 
                 Console.WriteLine("Choose an option:");
                 Console.WriteLine("1) Change Category");
@@ -213,38 +217,69 @@ namespace ASD_CW_2
                         Console.Write("Enter category: ");
                         string categoryName = Console.ReadLine();
 
+                        // Check category is exist or not
                         if (!categories.Any(category => category.getName().Equals(categoryName)))
                         {
                             Console.WriteLine($"Category {categoryName} is not available");
                         }
+                        else
+                        {
+                            // Update category with given category name
+                            transactionObj.setCategory(categories.FirstOrDefault(Category => Category.getName().Equals(categoryName)));
+
+                            Console.WriteLine($"Category change to {categoryName} Successfully");
+                        }
+
                         editTransaction();
                         break;
                     case 2:
                         Console.Write("Enter due-date (yyyy.mm.dd): ");
                         DateTime date = Convert.ToDateTime(Console.ReadLine());
+
+                        // Update transaction date and time
                         transactionObj.setDateTime(date);
+
+                        Console.WriteLine("Transaction Date/Time Updated Successfully");
+                        
                         editTransaction();
                         break;
                     case 3:
                         Console.Write("Enter transaction amount: ");
                         double amount = Convert.ToDouble(Console.ReadLine());
+
+                        // Update transaction amount
                         transactionObj.setAmount(amount);
+
+                        // Update budget balance if transaction category is expense
                         if (!transactionObj.getCategory().getType())
                         {
-                            transactionObj.getCategory().getBudget().setBalance(-amount); //=================================================updating the budget balance===========================
+                            transactionObj.getCategory().getBudget().setBalance(-amount); //updating the budget balance
                         }
+
+                        Console.WriteLine("Transaction Amount Updated Successfully");
+
                         editTransaction();
                         break;
                     case 4:
                         Console.Write("Enter description: ");
                         string desc = Console.ReadLine();
+
+                        // Update transaction description/note
                         transactionObj.setDescription(desc);
+
+                        Console.WriteLine("Transaction Description Updated Successfully");
+
                         editTransaction();
                         break;
                     case 5:
                         Console.Write("Is recurring (true or false): ");
                         bool recurring = Convert.ToBoolean(Console.ReadLine());
+
+                        // Update transaction recurring status
                         transactionObj.setRecurring(recurring);
+
+                        Console.WriteLine("Transaction Recurring Status Updated Successfully");
+
                         editTransaction();
                         break;
                     default:
@@ -258,13 +293,15 @@ namespace ASD_CW_2
             }
         }
 
+        // List recent transactions
         private static void listTransaction()
         {
             try
             {
                 foreach (Transaction t in transactions)
                 {
-                    string rec = t.isRecurring() ? "YES" : "NO";
+                    string rec = t.isRecurring() ? "YES" : "NO"; // transaction recurring status true and false values display as yes and no value
+
                     Console.WriteLine($"T_ID: {t.getId()} \tT_Date & Time: {t.getDate()} \tT_Amount: {t.getAmount()} \tT_Description: {t.getDescription()} \tT_Recurring: {rec} \tT_Category: {t.getCategory().getName()}");
                 }
             }
@@ -274,6 +311,7 @@ namespace ASD_CW_2
             }
         }
 
+        // Delete given transaction
         private static void deleteTransaction()
         {
             try
@@ -281,20 +319,26 @@ namespace ASD_CW_2
                 Console.Write("Enter the transaction Id you want to delete: ");
                 int id = Convert.ToInt32(Console.ReadLine());
 
-                Transaction transactionObj = transactions.FirstOrDefault(Transaction => Transaction.getId() == id);
-
-                if (transactionObj == null)
+                // Check transaction is exist or not
+                if (!transactions.Any(transaction => transaction.getId().Equals(id)))
                 {
                     Console.WriteLine($"Id number {id} not belongs to any transaction");
+                    menu();
                 }
 
-                //transactionObj.getCategory().getBudget().setBalance(transactionObj.getCategory().getBudget().getBalance() + transactionObj.getAmount());
+                // Get transaction object from list using id
+                Transaction transactionObj = transactions.FirstOrDefault(Transaction => Transaction.getId() == id);
+
+                // Update budget balance if transaction category is expense
                 if (!transactionObj.getCategory().getType())
                 {
                     transactionObj.getCategory().getBudget().setBalance(transactionObj.getAmount());
                 }
                 
+                // Remove transaction object from list
                 transactions.Remove(transactionObj);
+
+                Console.WriteLine($"Transaction {id} deleted successfully");
             }
             catch (Exception ex)
             {
@@ -302,17 +346,24 @@ namespace ASD_CW_2
             }
         }
 
+        // Add new category
         private static void addCategory()
         {
             try
             {
                 Console.Write("Enter category name: ");
                 string name = Console.ReadLine();
+
                 Console.Write("Enter category type (Income/Expense): ");
                 bool type = Console.ReadLine().Equals("Income") ? true : false;
+
+                // Check category is income type
                 if (type)
                 {
+                    // Create new category and set budget as zero
                     Category c1 = new Category(name, type, 0.0);
+
+                    // Add category to list
                     categories.Add(c1);
 
                 }
@@ -321,13 +372,18 @@ namespace ASD_CW_2
                     Console.Write("Enter category Budget: ");
                     double amount = Convert.ToDouble(Console.ReadLine());
 
+                    // Check total budget amount is exceed total Income
                     if ((getTotalBudget() + amount) > getTotalIncome())
                     {
                         Console.WriteLine("Total budget amount exceed income");
                     }
-
-                    Category c1 = new Category(name, type, amount);
-                    categories.Add(c1);
+                    else
+                    {
+                        // Create new category and set given budget amount
+                        Category c1 = new Category(name, type, amount);
+                        categories.Add(c1);
+                    }
+                    
                 }
             }
             catch(Exception ex)
@@ -336,7 +392,7 @@ namespace ASD_CW_2
             }
         }
 
-
+        // Edit existing category
         private static void editCategory()
         {
             try
@@ -344,15 +400,20 @@ namespace ASD_CW_2
                 Console.Write("Enter the Category Name You Want to Edit: ");
                 string cat_ID = Console.ReadLine();
 
-                Category catObj = categories.FirstOrDefault(Category => Category.getName() == cat_ID);
-
-                if (catObj == null)
+                // Check category is not exist
+                if (!categories.Any(category => category.getId().Equals(cat_ID)))
                 {
-                    Console.WriteLine($"Id number {cat_ID} not belongs to any transaction");
+                    Console.WriteLine($"Category {cat_ID} is not available");
+                    menu();
                 }
+                else
+                {
+                    // Get category object from list
+                    Category catObj = categories.FirstOrDefault(Category => Category.getName() == cat_ID);
 
-                changeCategory(catObj);
-                
+                    // Change category attributes
+                    changeCategory(catObj);
+                }
             }
             catch (Exception ex)
             {
@@ -360,6 +421,7 @@ namespace ASD_CW_2
             }
         }
 
+        // Change category attributes
         private static void changeCategory(Category catObj)
         {
             Console.WriteLine("Choose an option:");
@@ -378,27 +440,39 @@ namespace ASD_CW_2
                     Console.Write("Enter New Category Name: ");
                     string catName = Console.ReadLine();
 
+                    // Update category name
                     catObj.setName(catName);
+
+                    Console.WriteLine("Category name updated successfully");
+
                     changeCategory(catObj);
                     break;
                 case 2:
                     Console.Write("Enter the category Type: ");
-                    //string categoryType = Console.ReadLine();
-                    bool catType = Console.ReadLine().Equals("Income") ? true : false;
+                    bool catType = Console.ReadLine().Equals("Income") ? true : false; // Convert category type to boolean value
+
+                    // Update category type
                     catObj.setType(catType);
+
+                    Console.WriteLine("Category type updated successfully");
+
                     changeCategory(catObj);
                     break;
                 case 3:
                     Console.Write("Enter New category Budget: ");
                     double catAmount = Convert.ToDouble(Console.ReadLine());
 
+                    // Check total budget amount exceed total income
                     if ((getTotalBudget() + catAmount) > getTotalIncome())
                     {
                         Console.WriteLine("Total Budget Amount Exceeds Total Income");
                     }
                     else
                     {
+                        // Update budget amount
                         catObj.getBudget().setAmount(catAmount);
+
+                        Console.WriteLine("Budget Amount updated successfully");
                     }
 
                     changeCategory(catObj);
@@ -414,7 +488,7 @@ namespace ASD_CW_2
 
         }
 
-
+        // List categories
         private static void listCategory()
         {
             double total = 0;
@@ -424,33 +498,36 @@ namespace ASD_CW_2
                 Console.WriteLine("\nCategories:");
                 foreach (Category c in categories)
                 {
-                    string type = c.getType() ? "Income" : "Expense";
+                    string type = c.getType() ? "Income" : "Expense"; // Display budget type as Income or expense
+
                     if (!c.getType())
                     {
                         Console.WriteLine($"*C_Name: {c?.getName()}  \tC_Type: {type} \tC_Budget: {c.getBudget().getAmount()} \tC_Balance: {c.getBudget().getBalance()}");
                     }
                     else
                     {
-                        Console.WriteLine($"*C_Name: {c?.getName()}  \tC_Type: {type} \t\tC_Budget: N/A \tC_Balance: N/A");
+                        Console.WriteLine($"*C_Name: {c?.getName()}  \tC_Type: {type} \t\tC_Budget: N/A \tC_Balance: N/A"); // Display income type category budget balance as N/A
                     }
-                    //--------------------------------------------------------total balance value------------------
+
+                    // calculate total budget balance value
                     total += c.getBudget().getBalance();
-
-
-                    //------------------------------------------------------------------------------
                 }
+
+                Console.WriteLine(total);// Display total budget balance value
             }
             catch(Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            Console.WriteLine(total);//-------------------------------------------------------balance value----------------------------
+            
         }
 
+        // Get total income
         private static double getTotalIncome()
         {
             double total = 0.0;
-
+            
+            // Calculate total income value
             foreach (Transaction t in transactions)
             {
                 if (t.getCategory().getType())
@@ -462,10 +539,12 @@ namespace ASD_CW_2
             return total;
         }
 
+        // Get total budget amount
         private static double getTotalBudget()
         {
             double total = 0.0;
 
+            // Calculate total budget amount
             foreach (Category c in categories)
             {
                 total += c.getBudget().getAmount();
@@ -474,28 +553,35 @@ namespace ASD_CW_2
             return total;
         }
 
+        // Recurring transactions added as new transactions
         private static void addRecurringTransactions()
         {
             try
             {
                 List<Transaction> tempTransactions = new List<Transaction>();
-                DateTime previousDay = DateTime.Now.AddMinutes(-1);
+
+                // Get previous month
+                DateTime previousMonth = DateTime.Now.AddMinutes(-1); // demo purposes get previous minute instead of previous month
 
                 foreach (Transaction t in transactions)
                 {
-                    if (t.isRecurring() && t.getDate().ToString("g").Equals(previousDay.ToString("g")))
+                    // Get last month recurring transaction
+                    if (t.isRecurring() && t.getDate().ToString("g").Equals(previousMonth.ToString("g")))
                     {
+                        // Create new transaction object with previous transaction object values except id
                         Transaction t1 = new Transaction(t.getAmount(), t.getDescription(), t.isRecurring(), DateTime.Now, t.getCategory());
 
+                        // Update budget balance if transaction type is expense
                         if (!t.getCategory().getType())
                         {
-                            t.getCategory().getBudget().setBalance(-t.getAmount());
+                            t.getCategory().getBudget().setBalance(-t.getAmount()); // updating budget balance
                         }
 
-                        tempTransactions.Add(t1);
+                        tempTransactions.Add(t1); // add new transaction object to temp list
                     }
                 }
 
+                // Temp list transaction objects added to transaction list
                 foreach (Transaction t in tempTransactions)
                 {
                     transactions.Add(t);
